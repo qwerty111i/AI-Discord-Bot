7a617186175e8852114c3c34eeb0f511d454fe18
Whipshadow <47545327+Whipshadow@users.noreply.github.com>
2025-03-09 21:29:21 -0400

    change safety feature

--- commands/ask.js
+++ commands/ask.js
@@ -74,7 +74,7 @@ export async function askExecute(userInfo, prompt, guildId) {
       generationConfig,
       history: conversationHistory,
       safetySettings: [
-        { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+        { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_NONE },
         { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE },
         { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
         { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_NONE },
